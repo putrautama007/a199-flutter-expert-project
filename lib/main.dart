3c232221bdd3a5a1aff243c39ef52f05168e31d8
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-10 23:03:08 +0800

    refactor: update directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
-import 'package:ditonton/common/constants.dart';
-import 'package:ditonton/presentation/pages/about_page.dart';
-import 'package:ditonton/presentation/pages/movie_detail_page.dart';
-import 'package:ditonton/presentation/pages/home_movie_page.dart';
-import 'package:ditonton/presentation/pages/popular_movies_page.dart';
-import 'package:ditonton/presentation/pages/search_page.dart';
-import 'package:ditonton/presentation/pages/top_rated_movies_page.dart';
-import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
-import 'package:ditonton/presentation/provider/movie_detail_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
-import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/top_rated_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
+import 'package:ditonton/core/util/common/constants.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/about_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/home_movie_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/movie_detail_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/popular_movies_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/search_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/top_rated_movies_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/watchlist_movies_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/movie_detail_notifier.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/movie_list_notifier.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/movie_search_notifier.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/popular_movies_notifier.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/top_rated_movies_notifier.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ditonton/injection.dart' as di;
+import 'package:ditonton/core/util/di/injection.dart' as di;
 
 void main() {
   di.init();
@@ -51,7 +51,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           colorScheme: kColorScheme,
           primaryColor: kRichBlack,
-          accentColor: kMikadoYellow,
           scaffoldBackgroundColor: kRichBlack,
           textTheme: kTextTheme,
         ),
