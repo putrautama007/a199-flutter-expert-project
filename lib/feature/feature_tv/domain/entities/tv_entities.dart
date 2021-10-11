038eb4add05e7275e880e7eff07d247a7b3fa306
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 22:03:03 +0800

    feat: add tv entities

---
+++ lib/feature/feature_tv/domain/entities/tv_entities.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+
+class TvEntities extends Equatable {
+  final String? backdropPath;
+  final String? firstAirDate;
+  final List<int>? genreIds;
+  final List<String>? originCountry;
+  final int? id;
+  final String? originalLanguage;
+  final String? originalName;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  final double? voteAverage;
+  final int? voteCount;
+
+  TvEntities({
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genreIds,
+    required this.originCountry,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalName,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  @override
+  List<Object?> get props => [
+        backdropPath,
+        genreIds,
+        originalLanguage,
+        id,
+        originCountry,
+        originalName,
+        overview,
+        popularity,
+        posterPath,
+        voteAverage,
+        voteCount,
+      ];
+}
