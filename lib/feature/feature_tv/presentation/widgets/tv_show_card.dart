8da51f5084b7c53abc0b77a631c54bb50b54b8ce
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 22:46:25 +0800

    feat: add tv show card

---
+++ lib/feature/feature_tv/presentation/widgets/tv_show_card.dart
@@ -0,0 +1,79 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/core/util/constant/api_constants.dart';
+import 'package:ditonton/core/util/style/text_styles.dart';
+import 'package:ditonton/feature/feature_tv/domain/entities/tv_entities.dart';
+import 'package:flutter/material.dart';
+
+class TvShowCard extends StatelessWidget {
+  final TvEntities tvEntities;
+
+  const TvShowCard({
+    required this.tvEntities,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: InkWell(
+        onTap: () {
+          ///will update after make detail page
+          // Navigator.pushNamed(
+          //   context,
+          //   MovieDetailPage.ROUTE_NAME,
+          //   arguments: tvEntities.id,
+          // );
+        },
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Card(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  left: 16 + 80 + 16,
+                  bottom: 8,
+                  right: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tvEntities.originalName ?? '-',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: kHeading6,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      tvEntities.overview ?? '-',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+              ),
+              child: ClipRRect(
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tvEntities.posterPath}',
+                  width: 80,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
