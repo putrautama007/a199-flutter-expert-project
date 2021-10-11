faa1251bded886329b55819bcbb16f0363d05620
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 23:35:45 +0800

    add: add see more button

---
+++ lib/core/presentation/widgets/button/see_more_button.dart
@@ -0,0 +1,35 @@
+import 'package:ditonton/core/util/style/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SeeMoreButton extends StatelessWidget {
+  final String title;
+  final Function() onTap;
+
+  const SeeMoreButton({
+    required this.title,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: kHeading6,
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
