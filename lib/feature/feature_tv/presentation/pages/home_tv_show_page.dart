446392b292ec3159444d24582ee03e8d1ef707d9
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 23:49:59 +0800

    add: add home tv show page

---
+++ lib/feature/feature_tv/presentation/pages/home_tv_show_page.dart
@@ -0,0 +1,120 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/core/presentation/widgets/button/see_more_button.dart';
+import 'package:ditonton/core/presentation/widgets/scaffold/custom_scaffold.dart';
+import 'package:ditonton/core/util/common/state_enum.dart';
+import 'package:ditonton/core/util/constant/api_constants.dart';
+import 'package:ditonton/core/util/style/text_styles.dart';
+import 'package:ditonton/feature/feature_tv/domain/entities/tv_entities.dart';
+import 'package:ditonton/feature/feature_tv/presentation/provider/tv_show_list_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeTVShowPage extends StatelessWidget {
+  const HomeTVShowPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      searchRoute: "",
+      title: 'Ditonton Tv Show',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Now Playing',
+                style: kHeading6,
+              ),
+              Consumer<TvShowListNotifier>(builder: (context, data, child) {
+                final state = data.nowPlayingState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return tvShowList(data.nowPlayingTvShows);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Popular',
+                onTap: () => Navigator.pushNamed(context, ""),
+              ),
+              Consumer<TvShowListNotifier>(builder: (context, data, child) {
+                final state = data.popularState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return tvShowList(data.popularTvShows);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Top Rated',
+                onTap: () => Navigator.pushNamed(context, ""),
+              ),
+              Consumer<TvShowListNotifier>(builder: (context, data, child) {
+                final state = data.topRatedState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return tvShowList(data.topRatedTvShows);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSubHeading({required String title, required Function() onTap}) =>
+      SeeMoreButton(
+        title: title,
+        onTap: onTap,
+      );
+
+  Container tvShowList(List<TvEntities> tvShows) => Container(
+        height: 200,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            final movie = tvShows[index];
+            return Container(
+              padding: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: () {
+                  /// will be update after detail page create
+                  // Navigator.pushNamed(
+                  //   context,
+                  //   MovieDetailPage.ROUTE_NAME,
+                  //   arguments: movie.id,
+                  // );
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  child: CachedNetworkImage(
+                    imageUrl: '$BASE_IMAGE_URL${movie.posterPath}',
+                    placeholder: (context, url) => Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: tvShows.length,
+        ),
+      );
+}
