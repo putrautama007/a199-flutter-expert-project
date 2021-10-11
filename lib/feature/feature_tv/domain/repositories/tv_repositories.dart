8e916950a73ff2c86d4bbe587ae69935c522af8a
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 22:11:39 +0800

    refactor: change tv entities to tv model on get now playing, popular, top rated tv shows

--- lib/feature/feature_tv/domain/repositories/tv_repositories.dart
+++ lib/feature/feature_tv/domain/repositories/tv_repositories.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/core/util/common/failure.dart';
-import 'package:ditonton/feature/feature_tv/domain/entities/tv_entities.dart';
+import 'package:ditonton/feature/feature_tv/data/models/tv_model.dart';
 
 abstract class TvRepositories{
-  Future<Either<Failure, List<TvEntities>>> getNowPlayingTvShows();
-  Future<Either<Failure, List<TvEntities>>> getPopularTvShows();
-  Future<Either<Failure, List<TvEntities>>> getTopRatedTvShows();
+  Future<Either<Failure, List<TvModel>>> getNowPlayingTvShows();
+  Future<Either<Failure, List<TvModel>>> getPopularTvShows();
+  Future<Either<Failure, List<TvModel>>> getTopRatedTvShows();
 }
