7c9d5e1a6994bf75cb43f94988abe116604e6c76
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-11 23:19:33 +0800

    refactor: change drawer to bottom navigation

--- lib/feature/feature_home/presentation/pages/bottom_nav_page.dart
+++ lib/feature/feature_home/presentation/pages/bottom_nav_page.dart
@@ -1,12 +1,10 @@
-import 'package:ditonton/core/util/common/state_enum.dart';
-import 'package:ditonton/core/util/style/text_styles.dart';
+import 'package:ditonton/core/util/color/app_colors.dart';
+import 'package:ditonton/feature/feature_home/presentation/provider/bottom_nav_notifier.dart';
 import 'package:ditonton/feature/feature_movie/presentation/pages/about_page.dart';
 import 'package:ditonton/feature/feature_movie/presentation/pages/home_movie_page.dart';
-import 'package:ditonton/feature/feature_movie/presentation/pages/popular_movies_page.dart';
-import 'package:ditonton/feature/feature_movie/presentation/pages/search_page.dart';
-import 'package:ditonton/feature/feature_movie/presentation/pages/top_rated_movies_page.dart';
 import 'package:ditonton/feature/feature_movie/presentation/pages/watchlist_movies_page.dart';
 import 'package:ditonton/feature/feature_movie/presentation/provider/movie_list_notifier.dart';
+import 'package:ditonton/feature/feature_tv/presentation/pages/home_tv_show_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,7 +18,7 @@ class _BottomNavPageState extends State<BottomNavPage> {
   void initState() {
     super.initState();
     Future.microtask(
-            () => Provider.of<MovieListNotifier>(context, listen: false)
+        () => Provider.of<MovieListNotifier>(context, listen: false)
           ..fetchNowPlayingMovies()
           ..fetchPopularMovies()
           ..fetchTopRatedMovies());
@@ -28,133 +26,99 @@ class _BottomNavPageState extends State<BottomNavPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/circle-g.png'),
-              ),
-              accountName: Text('Ditonton'),
-              accountEmail: Text('ditonton@dicoding.com'),
-            ),
-            ListTile(
-              leading: Icon(Icons.movie),
-              title: Text('Movies'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.save_alt),
-              title: Text('Watchlist'),
-              onTap: () {
-                Navigator.pushNamed(context, WatchlistMoviesPage.ROUTE_NAME);
-              },
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
-              },
-              leading: Icon(Icons.info_outline),
-              title: Text('About'),
-            ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text('Ditonton'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, SearchPage.ROUTE_NAME);
-            },
-            icon: Icon(Icons.search),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Now Playing',
-                style: kHeading6,
-              ),
-              Consumer<MovieListNotifier>(builder: (context, data, child) {
-                final state = data.nowPlayingState;
-                if (state == RequestState.Loading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state == RequestState.Loaded) {
-                  return HomeMoviePage(data.nowPlayingMovies);
-                } else {
-                  return Text('Failed');
-                }
-              }),
-              _buildSubHeading(
-                title: 'Popular',
-                onTap: () =>
-                    Navigator.pushNamed(context, PopularMoviesPage.ROUTE_NAME),
-              ),
-              Consumer<MovieListNotifier>(builder: (context, data, child) {
-                final state = data.popularMoviesState;
-                if (state == RequestState.Loading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state == RequestState.Loaded) {
-                  return HomeMoviePage(data.popularMovies);
-                } else {
-                  return Text('Failed');
-                }
-              }),
-              _buildSubHeading(
-                title: 'Top Rated',
-                onTap: () =>
-                    Navigator.pushNamed(context, TopRatedMoviesPage.ROUTE_NAME),
-              ),
-              Consumer<MovieListNotifier>(builder: (context, data, child) {
-                final state = data.topRatedMoviesState;
-                if (state == RequestState.Loading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state == RequestState.Loaded) {
-                  return HomeMoviePage(data.topRatedMovies);
-                } else {
-                  return Text('Failed');
-                }
-              }),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+    // return Scaffold(
+    //   drawer: Drawer(
+    //     child: Column(
+    //       children: [
+    //         UserAccountsDrawerHeader(
+    //           currentAccountPicture: CircleAvatar(
+    //             backgroundImage: AssetImage('assets/circle-g.png'),
+    //           ),
+    //           accountName: Text('Ditonton'),
+    //           accountEmail: Text('ditonton@dicoding.com'),
+    //         ),
+    //         ListTile(
+    //           leading: Icon(Icons.movie),
+    //           title: Text('Movies'),
+    //           onTap: () {
+    //             Navigator.pop(context);
+    //           },
+    //         ),
+    //         ListTile(
+    //           leading: Icon(Icons.save_alt),
+    //           title: Text('Watchlist'),
+    //           onTap: () {
+    //             Navigator.pushNamed(context, WatchlistMoviesPage.ROUTE_NAME);
+    //           },
+    //         ),
+    //         ListTile(
+    //           onTap: () {
+    //             Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
+    //           },
+    //           leading: Icon(Icons.info_outline),
+    //           title: Text('About'),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
 
-  Row _buildSubHeading({required String title, required Function() onTap}) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          style: kHeading6,
-        ),
-        InkWell(
-          onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+    return Consumer<BottomNavNotifier>(
+      builder: (context, bottomNavNotifier, widget) {
+        return Scaffold(
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: IndexedStack(
+              index: bottomNavNotifier.tabIndex,
+              children: [
+                HomeMoviePage(),
+                HomeTVShowPage(),
+                WatchlistMoviesPage(),
+                AboutPage(),
+              ],
             ),
           ),
-        ),
-      ],
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38,
+                  spreadRadius: 0,
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: BottomNavigationBar(
+              showUnselectedLabels: true,
+              showSelectedLabels: true,
+              selectedItemColor: kWhite,
+              unselectedItemColor: kDavysGrey,
+              onTap: bottomNavNotifier.changeTabIndex,
+              currentIndex: bottomNavNotifier.tabIndex,
+              type: BottomNavigationBarType.fixed,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.movie),
+                  label: "Movies",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.tv),
+                  label: "Tv Shows",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.save_alt),
+                  label: "Watchlist",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.info_outline),
+                  label: "About",
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/feature/feature_movie/presentation/pages/home_movie_page.dart
+++ lib/feature/feature_movie/presentation/pages/home_movie_page.dart
@@ -1,49 +1,147 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/core/util/common/state_enum.dart';
 import 'package:ditonton/core/util/constant/api_constants.dart';
+import 'package:ditonton/core/util/style/text_styles.dart';
 import 'package:ditonton/feature/feature_movie/domain/entities/movie.dart';
 import 'package:ditonton/feature/feature_movie/presentation/pages/movie_detail_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/popular_movies_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/search_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/top_rated_movies_page.dart';
+import 'package:ditonton/feature/feature_movie/presentation/provider/movie_list_notifier.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeMoviePage extends StatelessWidget {
-  final List<Movie> movies;
-
-  HomeMoviePage(this.movies);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return Container(
-            padding: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  MovieDetailPage.ROUTE_NAME,
-                  arguments: movie.id,
-                );
-              },
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                child: CachedNetworkImage(
-                  imageUrl: '$BASE_IMAGE_URL${movie.posterPath}',
-                  placeholder: (context, url) => Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ditonton'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, SearchPage.ROUTE_NAME);
+            },
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Now Playing',
+                style: kHeading6,
               ),
-            ),
-          );
-        },
-        itemCount: movies.length,
+              Consumer<MovieListNotifier>(builder: (context, data, child) {
+                final state = data.nowPlayingState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return movieList(data.nowPlayingMovies);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Popular',
+                onTap: () =>
+                    Navigator.pushNamed(context, PopularMoviesPage.ROUTE_NAME),
+              ),
+              Consumer<MovieListNotifier>(builder: (context, data, child) {
+                final state = data.popularMoviesState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return movieList(data.popularMovies);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Top Rated',
+                onTap: () =>
+                    Navigator.pushNamed(context, TopRatedMoviesPage.ROUTE_NAME),
+              ),
+              Consumer<MovieListNotifier>(builder: (context, data, child) {
+                final state = data.topRatedMoviesState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return movieList(data.topRatedMovies);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  Row _buildSubHeading({required String title, required Function() onTap}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: kHeading6,
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container movieList(List<Movie> movies) => Container(
+        height: 200,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return Container(
+              padding: const EdgeInsets.all(8),
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    MovieDetailPage.ROUTE_NAME,
+                    arguments: movie.id,
+                  );
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  child: CachedNetworkImage(
+                    imageUrl: '$BASE_IMAGE_URL${movie.posterPath}',
+                    placeholder: (context, url) => Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: movies.length,
+        ),
+      );
 }
