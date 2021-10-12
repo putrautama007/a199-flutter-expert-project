e4b00302491f9ad67ad89cc1e811204db56f5c84
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 23:12:45 +0800

    feat: implement tv show search notifier on search tv show page

--- lib/feature/feature_tv/presentation/pages/search_tv_show_page.dart
+++ lib/feature/feature_tv/presentation/pages/search_tv_show_page.dart
@@ -1,11 +1,73 @@
+import 'package:ditonton/core/util/common/state_enum.dart';
+import 'package:ditonton/core/util/style/text_styles.dart';
+import 'package:ditonton/feature/feature_tv/presentation/provider/tv_show_search_notifier.dart';
+import 'package:ditonton/feature/feature_tv/presentation/widgets/tv_show_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SearchTvShowPage extends StatelessWidget {
   static const routeName = '/searchTvShow';
+
   const SearchTvShowPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              onSubmitted: (query) {
+                Provider.of<TvShowSearchNotifier>(context, listen: false)
+                    .fetchTvShowSearch(query);
+              },
+              decoration: InputDecoration(
+                hintText: 'Search title',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              textInputAction: TextInputAction.search,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Search Result',
+              style: kHeading6,
+            ),
+            Consumer<TvShowSearchNotifier>(
+              builder: (context, data, child) {
+                if (data.state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (data.state == RequestState.Loaded) {
+                  final result = data.searchResult;
+                  return Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemBuilder: (context, index) {
+                        final tvEntities = data.searchResult[index];
+                        return TvShowCard(
+                          tvEntities: tvEntities,
+                        );
+                      },
+                      itemCount: result.length,
+                    ),
+                  );
+                } else {
+                  return Expanded(
+                    child: Container(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
