3fba408e87f025341aefd5a27829458f46bc1328
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 20:06:37 +0800

    feat: add tv detail model, episode model, and season model

---
+++ lib/feature/feature_tv/data/models/episode_model.dart
@@ -0,0 +1,55 @@
+
+import 'package:equatable/equatable.dart';
+
+class EpisodeModel extends Equatable {
+  EpisodeModel({
+    required this.airDate,
+    required this.episodeNumber,
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.productionCode,
+    required this.seasonNumber,
+    required this.stillPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final String airDate;
+  final int episodeNumber;
+  final int id;
+  final String name;
+  final String overview;
+  final String productionCode;
+  final int seasonNumber;
+  final String stillPath;
+  final double voteAverage;
+  final int voteCount;
+
+  factory EpisodeModel.fromJson(Map<String, dynamic> json) => EpisodeModel(
+    airDate: json["air_date"],
+    episodeNumber: json["episode_number"],
+    id: json["id"],
+    name: json["name"],
+    overview: json["overview"],
+    productionCode: json["production_code"],
+    seasonNumber: json["season_number"],
+    stillPath: json["still_path"],
+    voteAverage: json["vote_average"].toDouble(),
+    voteCount: json["vote_count"],
+  );
+
+  @override
+  List<Object?> get props => [
+    airDate,
+    episodeNumber,
+    id,
+    name,
+    overview,
+    productionCode,
+    seasonNumber,
+    stillPath,
+    voteAverage,
+    voteCount,
+  ];
+}
---
+++ lib/feature/feature_tv/data/models/season_model.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class SeasonModel extends Equatable {
+  SeasonModel({
+    required this.airDate,
+    required this.episodeCount,
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.posterPath,
+    required this.seasonNumber,
+  });
+
+  final String airDate;
+  final int episodeCount;
+  final int id;
+  final String name;
+  final String overview;
+  final String posterPath;
+  final int seasonNumber;
+
+  factory SeasonModel.fromJson(Map<String, dynamic> json) => SeasonModel(
+    airDate: json["air_date"],
+    episodeCount: json["episode_count"],
+    id: json["id"],
+    name: json["name"],
+    overview: json["overview"],
+    posterPath: json["poster_path"],
+    seasonNumber: json["season_number"],
+  );
+
+  @override
+  List<Object?> get props => [
+    airDate,
+    episodeCount,
+    id,
+    name,
+    overview,
+    posterPath,
+    seasonNumber,
+  ];
+}
