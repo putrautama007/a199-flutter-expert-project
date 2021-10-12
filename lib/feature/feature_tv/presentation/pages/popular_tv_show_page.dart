6ea833a261229dd23920713dfb396c208ee507d3
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 23:24:06 +0800

    feat: add popular and top rated tv show page

---
+++ lib/feature/feature_tv/presentation/pages/popular_tv_show_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PopularTvShowPage extends StatefulWidget {
+  static const routeName = '/popularTvShow';
+  const PopularTvShowPage({Key? key}) : super(key: key);
+
+  @override
+  _PopularTvShowPageState createState() => _PopularTvShowPageState();
+}
+
+class _PopularTvShowPageState extends State<PopularTvShowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/feature/feature_tv/presentation/pages/top_rated_tv_show_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TopRatedTvShowPage extends StatefulWidget {
+  static const routeName = '/topRatedTvShow';
+
+  const TopRatedTvShowPage({Key? key}) : super(key: key);
+
+  @override
+  _TopRatedTvShowPageState createState() => _TopRatedTvShowPageState();
+}
+
+class _TopRatedTvShowPageState extends State<TopRatedTvShowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
