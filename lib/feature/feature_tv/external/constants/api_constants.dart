081bd7bbb4c1679d289dfbf2cdae3ff6163c0881
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 20:38:06 +0800

    feat: add recommendation tv api constants

--- lib/feature/feature_tv/external/constants/api_constants.dart
+++ lib/feature/feature_tv/external/constants/api_constants.dart
@@ -3,5 +3,6 @@ class ApiConstants {
   static const String tvPopular = "/tv/popular";
   static const String tvTopRated = "/tv/top_rated";
 
-  static String detailTv({required String tvId}) => "/tv/$tvId}";
+  static String detailTv({required String tvId}) => "/tv/$tvId";
+  static String recommendationTv({required String tvId}) => "/tv/$tvId/recommendations";
 }
