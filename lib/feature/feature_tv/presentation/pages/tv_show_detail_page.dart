a45f845423ed27d77dc298fdb05e681d061f16b4
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 22:03:23 +0800

    refactor: change tv show detail page to stateful widget

--- lib/feature/feature_tv/presentation/pages/tv_show_detail_page.dart
+++ lib/feature/feature_tv/presentation/pages/tv_show_detail_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TvShowDetailPage extends StatelessWidget {
+class TvShowDetailPage extends StatefulWidget {
   static const routeName = '/tvShowDetailPage';
 
   final String tvId;
 
   const TvShowDetailPage({required this.tvId, Key? key}) : super(key: key);
 
+  @override
+  State<TvShowDetailPage> createState() => _TvShowDetailPageState();
+}
+
+class _TvShowDetailPageState extends State<TvShowDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Container();
