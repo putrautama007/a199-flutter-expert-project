a05dbca83c490f72b130fbc6ff45c42bfa1d1400
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 23:06:20 +0800

    feat: add search tv show page

---
+++ lib/feature/feature_tv/presentation/pages/search_tv_show_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchTvShowPage extends StatelessWidget {
+  const SearchTvShowPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
