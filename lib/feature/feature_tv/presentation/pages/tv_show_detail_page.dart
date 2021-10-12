9b09661fa322ae562314b917d090661544cc6525
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 22:00:45 +0800

    feat: add tv show detail page

---
+++ lib/feature/feature_tv/presentation/pages/tv_show_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TvShowDetailPage extends StatelessWidget {
+  static const routeName = '/tvShowDetailPage';
+
+  final int id;
+
+  const TvShowDetailPage({required this.id, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
