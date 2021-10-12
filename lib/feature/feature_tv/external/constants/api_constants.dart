23d6cd5636db278d8eddefe977a855363254b91e
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 22:42:42 +0800

    feat: add search tv show end point

--- lib/feature/feature_tv/external/constants/api_constants.dart
+++ lib/feature/feature_tv/external/constants/api_constants.dart
@@ -2,6 +2,7 @@ class ApiConstants {
   static const String tvOnAir = "/tv/on_the_air";
   static const String tvPopular = "/tv/popular";
   static const String tvTopRated = "/tv/top_rated";
+  static const String tvSearch = "/search/tv";
 
   static String detailTv({required String tvId}) => "/tv/$tvId";
   static String recommendationTv({required String tvId}) => "/tv/$tvId/recommendations";
