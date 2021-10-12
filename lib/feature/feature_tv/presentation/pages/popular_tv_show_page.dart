ea9023843d7559e4035a846bc8576527914258de
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 23:27:42 +0800

    feat: implement provider on popular and top rated tv show page

--- lib/feature/feature_tv/presentation/pages/popular_tv_show_page.dart
+++ lib/feature/feature_tv/presentation/pages/popular_tv_show_page.dart
@@ -1,7 +1,12 @@
+import 'package:ditonton/core/util/common/state_enum.dart';
+import 'package:ditonton/feature/feature_tv/presentation/provider/tv_show_popular_notifier.dart';
+import 'package:ditonton/feature/feature_tv/presentation/widgets/tv_show_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PopularTvShowPage extends StatefulWidget {
   static const routeName = '/popularTvShow';
+
   const PopularTvShowPage({Key? key}) : super(key: key);
 
   @override
@@ -10,7 +15,46 @@ class PopularTvShowPage extends StatefulWidget {
 
 class _PopularTvShowPageState extends State<PopularTvShowPage> {
   @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<TvShowPopularNotifier>(context, listen: false)
+            .fetchPopularTvShows());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Popular Tv Shows'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<TvShowPopularNotifier>(
+          builder: (context, data, child) {
+            if (data.state == RequestState.Loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (data.state == RequestState.Loaded) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final tvShow = data.tvShow[index];
+                  return TvShowCard(
+                    tvEntities: tvShow,
+                  );
+                },
+                itemCount: data.tvShow.length,
+              );
+            } else {
+              return Center(
+                key: Key('error_message'),
+                child: Text(data.message),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/feature/feature_tv/presentation/pages/top_rated_tv_show_page.dart
+++ lib/feature/feature_tv/presentation/pages/top_rated_tv_show_page.dart
@@ -1,4 +1,8 @@
+import 'package:ditonton/core/util/common/state_enum.dart';
+import 'package:ditonton/feature/feature_tv/presentation/provider/tv_show_top_rated_notifier.dart';
+import 'package:ditonton/feature/feature_tv/presentation/widgets/tv_show_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TopRatedTvShowPage extends StatefulWidget {
   static const routeName = '/topRatedTvShow';
@@ -11,7 +15,46 @@ class TopRatedTvShowPage extends StatefulWidget {
 
 class _TopRatedTvShowPageState extends State<TopRatedTvShowPage> {
   @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<TvShowTopRatedNotifier>(context, listen: false)
+            .fetchTopRatedTvShows());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top Rated Movies'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<TvShowTopRatedNotifier>(
+          builder: (context, data, child) {
+            if (data.state == RequestState.Loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (data.state == RequestState.Loaded) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final tvShow = data.tvShow[index];
+                  return TvShowCard(
+                    tvEntities: tvShow,
+                  );
+                },
+                itemCount: data.tvShow.length,
+              );
+            } else {
+              return Center(
+                key: Key('error_message'),
+                child: Text(data.message),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
