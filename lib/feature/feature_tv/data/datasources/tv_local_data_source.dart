b8a059bc44bcbb4e8c258523ff3ef61af1d0875e
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-12 21:14:30 +0800

    feat: add tv local data source

---
+++ lib/feature/feature_tv/data/datasources/tv_local_data_source.dart
@@ -0,0 +1,55 @@
+import 'package:ditonton/core/data/models/watch_list_table.dart';
+import 'package:ditonton/core/util/common/exception.dart';
+import 'package:ditonton/core/util/db/database_helper.dart';
+
+abstract class TvLocalDataSource {
+  Future<String> insertWatchlist(WatchListTable tvShow);
+
+  Future<String> removeWatchlist(WatchListTable tvShow);
+
+  Future<WatchListTable?> getMovieById(int id);
+
+  Future<List<WatchListTable>> getWatchlistMovies();
+}
+
+class TvLocalDataSourceImpl implements TvLocalDataSource {
+  final DatabaseHelper databaseHelper;
+
+  TvLocalDataSourceImpl({required this.databaseHelper});
+
+  @override
+  Future<String> insertWatchlist(WatchListTable tvShow) async {
+    try {
+      await databaseHelper.insertTvShowWatchlist(tvShow);
+      return 'Added to Watchlist';
+    } catch (e) {
+      throw DatabaseException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> removeWatchlist(WatchListTable tvShow) async {
+    try {
+      await databaseHelper.insertTvShowWatchlist(tvShow);
+      return 'Removed from Watchlist';
+    } catch (e) {
+      throw DatabaseException(e.toString());
+    }
+  }
+
+  @override
+  Future<WatchListTable?> getMovieById(int id) async {
+    final result = await databaseHelper.getMovieById(id);
+    if (result != null) {
+      return WatchListTable.fromMap(result);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<List<WatchListTable>> getWatchlistMovies() async {
+    final result = await databaseHelper.getWatchlistMovies();
+    return result.map((data) => WatchListTable.fromMap(data)).toList();
+  }
+}
