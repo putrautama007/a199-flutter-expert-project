54a52344fff461dc807d872dbf68c44dd864169f
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-13 15:24:56 +0800

    feat: add tab bar on watch list page

--- lib/feature/feature_home/presentation/pages/watch_list_page.dart
+++ lib/feature/feature_home/presentation/pages/watch_list_page.dart
@@ -1,3 +1,6 @@
+import 'package:ditonton/core/util/color/app_colors.dart';
+import 'package:ditonton/feature/feature_movie/presentation/pages/watchlist_movies_page.dart';
+import 'package:ditonton/feature/feature_tv/presentation/pages/tv_show_watch_list_page.dart';
 import 'package:flutter/material.dart';
 
 class WatchListPage extends StatelessWidget {
@@ -5,6 +8,36 @@ class WatchListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Watchlist'),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            indicatorWeight: 5.0,
+            labelColor: kWhite,
+            labelPadding: const EdgeInsets.only(top: 10.0),
+            unselectedLabelColor: kDavysGrey,
+            tabs: const [
+              Tab(
+                text: 'Movie',
+                iconMargin: EdgeInsets.only(bottom: 10.0),
+              ),
+              Tab(
+                text: 'Tv Show',
+                iconMargin: EdgeInsets.only(bottom: 10.0),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            WatchlistMoviesPage(),
+            TvShowWatchListPage(),
+          ],
+        ),
+      ),
+    );
   }
 }
