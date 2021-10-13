dd0e32b8d91bd8a03ed8396aea319f7c969d36f6
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-13 22:03:25 +0800

    test: update test helper mock and generate new mock

--- test/core/helpers/test_helper.dart
+++ test/core/helpers/test_helper.dart
@@ -2,6 +2,9 @@ import 'package:ditonton/core/util/db/database_helper.dart';
 import 'package:ditonton/feature/feature_movie/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/feature/feature_movie/data/datasources/movie_remote_data_source.dart';
 import 'package:ditonton/feature/feature_movie/domain/repositories/movie_repository.dart';
+import 'package:ditonton/feature/feature_tv/data/datasources/tv_local_data_source.dart';
+import 'package:ditonton/feature/feature_tv/data/datasources/tv_remote_data_source.dart';
+import 'package:ditonton/feature/feature_tv/domain/repositories/tv_repositories.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +13,9 @@ import 'package:http/http.dart' as http;
   MovieRemoteDataSource,
   MovieLocalDataSource,
   DatabaseHelper,
+  TvRemoteDataSource,
+  TvRepositories,
+  TvLocalDataSource,
 ], customMocks: [
   MockSpec<http.Client>(as: #MockHttpClient)
 ])
