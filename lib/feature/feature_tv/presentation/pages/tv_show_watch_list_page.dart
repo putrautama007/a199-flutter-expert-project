a8ec0038fd86c321145d4b7904e2e97fa9f1cf8e
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-13 14:36:20 +0800

    feat: add tv show watch list page

---
+++ lib/feature/feature_tv/presentation/pages/tv_show_watch_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TvShowWatchListPage extends StatefulWidget {
+  static const routeName = '/watchListTvShow';
+  const TvShowWatchListPage({Key? key}) : super(key: key);
+
+  @override
+  _TvShowWatchListPageState createState() => _TvShowWatchListPageState();
+}
+
+class _TvShowWatchListPageState extends State<TvShowWatchListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
