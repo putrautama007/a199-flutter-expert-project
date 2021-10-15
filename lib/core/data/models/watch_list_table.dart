c2a9b33a597075504163404ac1464239e4ac3e2d
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-15 13:09:08 +0800

    refactor: adjust watch list table to clear lint warning

--- lib/core/data/models/watch_list_table.dart
+++ lib/core/data/models/watch_list_table.dart
@@ -10,7 +10,7 @@ class WatchListTable extends Equatable {
   final String? posterPath;
   final String? overview;
 
-  WatchListTable({
+  const WatchListTable({
     required this.id,
     required this.title,
     required this.posterPath,
