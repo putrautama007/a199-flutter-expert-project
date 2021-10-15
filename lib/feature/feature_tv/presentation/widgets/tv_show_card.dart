20f5fd92aa8ded9746f6429ca9fedb86da5b1a74
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-15 13:50:19 +0800

    refactor: adjust tv show card to clear lint warning

--- lib/feature/feature_tv/presentation/widgets/tv_show_card.dart
+++ lib/feature/feature_tv/presentation/widgets/tv_show_card.dart
@@ -44,7 +44,7 @@ class TvShowCard extends StatelessWidget {
                       overflow: TextOverflow.ellipsis,
                       style: kHeading6,
                     ),
-                    SizedBox(height: 16),
+                    const SizedBox(height: 16),
                     Text(
                       tvEntities.overview ?? '-',
                       maxLines: 2,
@@ -63,12 +63,12 @@ class TvShowCard extends StatelessWidget {
                 child: CachedNetworkImage(
                   imageUrl: '$baseImageUrl${tvEntities.posterPath}',
                   width: 80,
-                  placeholder: (context, url) => Center(
+                  placeholder: (context, url) => const Center(
                     child: CircularProgressIndicator(),
                   ),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
               ),
             ),
           ],
