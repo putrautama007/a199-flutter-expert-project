eb83ca23f477d4b8d4aed74eea5fd8be147b5381
Putra Utama <nyoman.utama@amarbank.co.id>
2021-10-27 22:53:57 +0800

    feat: add export library package

--- lib/module/libraries/lib/libraries.dart
+++ lib/module/libraries/lib/libraries.dart
@@ -1,7 +1,8 @@
-library libraries;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:equatable/equatable.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:flutter_rating_bar/flutter_rating_bar.dart';
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:sqflite/sqflite.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
