0022e050cdd37026d2794440fc54609fe6923ffb
Putra Utama <nyoman.utama@amarbank.co.id>
2021-11-07 12:50:07 +0800

    feat: implement bottom nav bloc on bottom nav page

--- lib/module/feature_home/lib/feature_home.dart
+++ lib/module/feature_home/lib/feature_home.dart
@@ -1,4 +1,5 @@
 import 'package:core/core.dart';
+import 'package:feature_home/presentation/bloc/bottom_nav_bloc.dart';
 import 'package:libraries/libraries.dart';
 import 'presentation/pages/bottom_nav_page.dart';
 
@@ -10,7 +11,10 @@ class FeatureHome extends Module {
   List<ModularRoute> get routes => [
         ChildRoute(
           MainRoutes.home,
-          child: (_, __) => const BottomNavPage(),
+          child: (_, __) => BlocProvider(
+            create: (_) => BottomNavBloc(initialState: 0),
+            child: const BottomNavPage(),
+          ),
         ),
       ];
 }
--- lib/module/feature_home/lib/presentation/pages/bottom_nav_page.dart
+++ lib/module/feature_home/lib/presentation/pages/bottom_nav_page.dart
@@ -1,6 +1,7 @@
 import 'package:core/core.dart';
+import 'package:feature_home/presentation/bloc/bottom_nav_bloc.dart';
+import 'package:feature_home/presentation/bloc/bottom_nav_event.dart';
 import 'package:feature_home/presentation/pages/watch_list_page.dart';
-import 'package:feature_home/presentation/provider/bottom_nav_notifier.dart';
 import 'package:feature_movie/presentation/pages/about_page.dart';
 import 'package:feature_movie/presentation/pages/home_movie_page.dart';
 import 'package:feature_movie/presentation/provider/movie_list_notifier.dart';
@@ -9,6 +10,7 @@ import 'package:feature_tv/presentation/pages/home_tv_show_page.dart';
 import 'package:feature_tv/presentation/provider/tv_show_list_notifier.dart';
 import 'package:feature_tv/presentation/provider/tv_show_watchlist_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:libraries/libraries.dart';
 import 'package:provider/provider.dart';
 
 class BottomNavPage extends StatefulWidget {
@@ -45,14 +47,14 @@ class _BottomNavPageState extends State<BottomNavPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<BottomNavNotifier>(
-      builder: (context, bottomNavNotifier, widget) {
+    return BlocBuilder<BottomNavBloc, int>(
+      builder: (context, state) {
         return Scaffold(
           body: SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: IndexedStack(
-              index: bottomNavNotifier.tabIndex,
+              index: BlocProvider.of<BottomNavBloc>(context).state,
               children: const [
                 HomeMoviePage(),
                 HomeTVShowPage(),
@@ -76,8 +78,10 @@ class _BottomNavPageState extends State<BottomNavPage> {
               showSelectedLabels: true,
               selectedItemColor: kWhite,
               unselectedItemColor: kDavysGrey,
-              onTap: bottomNavNotifier.changeTabIndex,
-              currentIndex: bottomNavNotifier.tabIndex,
+              onTap: (value){
+                BlocProvider.of<BottomNavBloc>(context).add(ChangeTabEvent(state: value));
+              },
+              currentIndex: BlocProvider.of<BottomNavBloc>(context).state,
               type: BottomNavigationBarType.fixed,
               items: const <BottomNavigationBarItem>[
                 BottomNavigationBarItem(
